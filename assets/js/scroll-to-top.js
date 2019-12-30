ead82719d74aa10cec09c7bf7441b4f94eac21c3
Suyash <in.suyash@gmail.com>
2019-12-31 00:31:01 +0530

    added scroll to top button & replaces jquery 3.3.1 slim with complete jquery 3.3.1 library

---
+++ assets/js/scroll-to-top.js
@@ -0,0 +1,21 @@
+/* 
+	js for scroll to top.
+	https://codepen.io/michalwyrwa/pen/GBaPPj
+*/
+
+window.onload = function() {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+}
